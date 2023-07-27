b335fd7f49b00e7dc09bb9104f1658c1c8d2a64c
Szesny32 <bartoszszczesny.studia@gmail.com>
2023-07-27 18:11:18 +0200

    Update main.py

--- K-Nearest Neighbors/main.py
+++ K-Nearest Neighbors/main.py
@@ -4,10 +4,14 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 data = load_breast_cancer()
 # print(data)
-print("--------------------------------")
-print("Features: ", data.feature_names)
-print("--------------------------------")
-print("Targets: ", data.target_names)
+# print("--------------------------------")
+# print("Features: ", data.feature_names)
+# print("--------------------------------")
+# print("Targets: ", data.target_names)
+# print("--------------------------------")
+# print("Data: ", data.data)
+# print("--------------------------------")
+# print("data-targets: ", data.target)
 
 x_train, x_test, y_train, y_test = train_test_split(np.array(data.data),np.array(data.target), test_size = 0.2)
 
@@ -30,3 +34,17 @@ print("Correct: ", correct, "/",n)
 wrong = n-correct
 print("Wrong: ", wrong, "/",n)
 print("Model accuracy: ", score, "%")
+
+print("--------------------------------")
+print("2. For a two-dimensional vector space, a training set divided into two classes is given: ")
+points = [[1, 1], [2, 2], [1, 3], [3, 5], [2, 3], [4, 3], [5, 2]]
+point_classes = [1, 1, 1, 1, 2, 2, 2]
+for (point, point_class) in zip(points, point_classes) :
+    print("Point", point,"has class:", point_class)
+point = [3, 3]
+K = 3
+print("Classify element ", point, " using the nearest neighbor classifier KNN. Take K =",K)
+clf = KNeighborsClassifier(n_neighbors = K)
+clf.fit(points, point_classes)
+
+print("Point", point,"has class: ", clf.predict([point])[0])
