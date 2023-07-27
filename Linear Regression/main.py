cf69e07d83d203b24f8e182f7acf363943beaecf
Szesny32 <bartoszszczesny.studia@gmail.com>
2023-07-27 15:14:06 +0200

    ref

--- Linear Regression/main.py
+++ Linear Regression/main.py
@@ -40,13 +40,17 @@ print("Score if you study for 56 hours: ", model.predict(np.array([56]).reshape(
 
 exam_time_studied = np.array([20, 50, 32, 65, 23, 43, 10, 5, 22, 35, 29, 5, 56]).reshape(-1,1)
 exexam_scores = np.array([56, 83, 47, 93, 47, 82, 45, 23, 55, 67, 57, 4, 89]).reshape(-1,1)
-exam_time_studied_train, exam_time_studied_test, exam_score_train, exam_score_test = train_test_split(exam_time_studied, exexam_scores, test_size=0.4)
+
+
+
+best_exam_time_studied_train, best_exam_time_studied_test, best_exam_score_train, best_exam_score_test = train_test_split(exam_time_studied, exexam_scores, test_size=0.4)
+
 
 best_model = LinearRegression()
-best_model.fit(exam_time_studied_train, exam_score_train)
-best_score = best_model.score(exam_time_studied_test, exam_score_test) * 100
+best_model.fit(best_exam_time_studied_train, best_exam_score_train)
+best_score = best_model.score(best_exam_time_studied_test, best_exam_score_test) * 100
 
-for i in range(1,10001):
+for i in range(1,10000):
     exam_time_studied_train, exam_time_studied_test, exam_score_train, exam_score_test = train_test_split(exam_time_studied, exexam_scores, test_size=0.4)
     model = LinearRegression()
     model.fit(exam_time_studied_train, exam_score_train)
@@ -54,13 +58,27 @@ for i in range(1,10001):
     if score > best_score :
         best_score = score
         best_model = model
+        best_exam_time_studied_train = exam_time_studied_train
+        best_exam_time_studied_test = exam_time_studied_test
+        best_exam_score_train = exam_score_train
+        best_exam_score_test = exam_score_test
     print(i, score, "%")
 
-# Listing the coefficients of f(x) = ax + b for the best model
-print("Best model: ", best_score, "%")
 
+plt.scatter(best_exam_time_studied_train, best_exam_score_train)
+plt.ylim(0,100)
+plt.title("Base data")
+plt.show()
+
+
+
+print("Best model: ", best_score, "%")
 print("f(x) = ", best_model.coef_[0][0], "x + ", best_model.intercept_[0])
-plt.scatter(exam_time_studied_train, exam_score_train)
+print("Test data: ", best_exam_time_studied_test)
+print("Predict values: ", best_model.predict(best_exam_time_studied_test))
+print("True values: ", best_exam_score_test)
+
+plt.scatter(best_exam_time_studied_train, best_exam_score_train)
 plt.plot(np.linspace(0,70,100).reshape(-1,1), best_model.predict(np.linspace(0,70,100).reshape(-1,1)), 'r')
 plt.title("Best of 10000 with with training and test data ")
 plt.show()
